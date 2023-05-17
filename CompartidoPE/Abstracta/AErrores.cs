28d119ae76517c9fdbe3235acda66854c86a38c8
Juan Pablo <totomarion@gmail.com>
2023-05-17 16:57:21 -0300

    Corregir NroError

--- CompartidoPE/Abstracta/AErrores.cs
+++ CompartidoPE/Abstracta/AErrores.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Reflection;
 using System.Text;
 
 namespace AuthPeDDD.Compartido.Abstracta
@@ -7,25 +7,27 @@ namespace AuthPeDDD.Compartido.Abstracta
     public abstract class AErrores
     {
         private readonly Enum _enumValor;
+        private FieldInfo _fieldInfo;
         public AErrores(Enum enumValor)
         {
             _enumValor = enumValor;
+            _fieldInfo = _enumValor.GetType().GetField(_enumValor.ToString());
         }
         public string GetNroError()
         {
             StringBuilder NroError = new StringBuilder();
-            NroError.Append(_enumValor.ToString());
+            NroError.Append(_fieldInfo.FieldType.Name.Replace("Enum", ""));
+            NroError.Append(_enumValor.ToString("d"));
 
             return NroError.ToString();
         }
         public string GetDescription()
         {
             var description = _enumValor.ToString();
-            var fieldInfo = _enumValor.GetType().GetField(_enumValor.ToString());
 
-            if (fieldInfo != null)
+            if (_fieldInfo != null)
             {
-                var attrs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                var attrs = _fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
                 if (attrs != null && attrs.Length > 0)
                 {
                     description = ((DescriptionAttribute)attrs[0]).Description;
