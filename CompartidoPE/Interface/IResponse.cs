bf535858cf6d1851d0b51366a7805062d3c47da8
Juan Pablo Marion <totomarion@gmail.com>
2026-06-22 19:21:04 -0300

    Crear nuevas interface

--- CompartidoPE/Interface/IResponse.cs
+++ CompartidoPE/Interface/IResponse.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CompartidoPE.Interface
+﻿namespace CompartidoPE.Interface
 {
     public interface IResponse<T>
     {
-        public IList<T> Data { get; set; }
-        public IError Error { get; set; }
+        public IList<T>? Datas { get; set; }
+        public T? Data { get; set; }
+        public IError? Error { get; set; }
     }
 }
