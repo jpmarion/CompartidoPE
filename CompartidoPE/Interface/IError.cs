66bb2685702a8a1bb3c3cfad58978de45d51bbc6
Juan Pablo Marion <totomarion@gmail.com>
2026-06-22 20:36:27 -0300

    Corregir nulos

--- CompartidoPE/Interface/IError.cs
+++ CompartidoPE/Interface/IError.cs
@@ -2,7 +2,7 @@
 {
     public interface IError
     {
-        public string NroError { get; set; }
-        public string MsgError { get; set; }
+        public string? NroError { get; set; }
+        public string? MsgError { get; set; }
     }
 }
--- CompartidoPE/Modelo/Error.cs
+++ CompartidoPE/Modelo/Error.cs
@@ -4,7 +4,7 @@ namespace CompartidoPE.Modelo
 {
     public class Error : IError
     {
-        public string NroError { get; set; }
-        public string MsgError { get; set; }
+        public string? NroError { get; set; }
+        public string? MsgError { get; set; }
     }
 }
